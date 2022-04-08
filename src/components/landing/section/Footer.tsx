02d18fe081aa3aedc0f8549889cc98a1a118a48f
mojtaba-borzu <mojtaba.borzu69@gmail.com>
2022-04-08 04:41:48 +0430

    reafactoring

--- src/components/landing/section/Footer.tsx
+++ src/components/landing/section/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="w-full h-[100px] bg-[#212121] flex items-center justify-center text-white">
-      © 2022 BUGATTI AUTOMOBILES S.A.S.
+      © 2022 BUGATTI AUTOMOBILES S.A.S.s
     </div>
   );
 }
